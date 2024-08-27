c3f2d6066b0e29e1712b50c258ff0fefeaac7358
vampycat237 <98717915+vampycat237@users.noreply.github.com>
2024-08-27 13:34:47 -0600

    fixed cover art errors, updated readme
    README.md
    - actually described the theme in the readme
    - once again fixed a vanishing piece of the readme
    - added a step to setup to enable theme js
    - changed notes section to color schemes section, which now describes all color schemes
    - added bugs section for bug reporting info
    
    theme.js
    - fixed lyrics image fetching not working in most cases.
      - the extension will try to fetch the image URI through track metadata first.
      - failing that, it will fetch the URI from one of the cover art images displayed on-screen.
      - failing *that,* it will use a placeholder image (defaultCoverArt)
    - improved documentation of methods
    - renamed "assignTintValues" to "setFilterProperties" to better describe what it does (sets the spice-filter css properties)

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # vamptify
 hihi welcome to my first spicetify theme, currently called Vamptify!
-Vamptify includes a little extension to help it do some fancy stuff like setting the background of the lyrics page to the song's album cover, and... tinting the spotify green graphics to match the theme colors. yup.
-this document is here to walk you through setting up the theme so it works properly and such :o should be pretty straightforward!!
+this theme's main goal is to suit my personal tastes - fun colors, a bit more compact than modern spotify.
+It also adds a few spin animations, including a "record spin" animation on the play button and user avatars. Turning on reduced motion should disable this!
+Some buttons, most notably the back/forward history buttons, are moved from their default position because i didn't like where they were.
+
+Vamptify includes a little extension which you'll probably want to enable! The extension does the following:
+- sets the background of the lyrics page to the current song's album cover
+- tells the theme how to tint the spotify green graphics to match your colorscheme
 
 # setup instructions
 without further ado, here are the setup instructions! [make sure you have spicetify installed first](https://spicetify.app/docs/getting-started#installation)
@@ -9,15 +14,32 @@ without further ado, here are the setup instructions! [make sure you have spicet
 - extract the .zip to a new folder in spicetify's Themes directory (https://spicetify.app/docs/development/themes)
 - pick a color scheme! open up colors.ini with anything that can open text files (notepad will work just fine), and look for the stuff in [brackets] - those are the names of the colorschemes! right below the name is a description of what it should look like.
 - open up powershell (you can either search the start menu or do Windows+R and type "powershell"!)
-- enter the following one line at a time, replacing <your-color-scheme> with the name of the color scheme you choose (no brackets):
+- enter the following one line at a time, replacing \<your-color-scheme\> with the name of the color scheme you choose (no brackets):
 
 <code>spicetify config current_theme Vamptify
  spicetify config color_scheme \<your-color-scheme\>
+ spicetify config inject_theme_js 1
  spicetify apply</code>
 
 - and you should be all good!
 - (if spicetify suggests you do another command first before any step, go ahead and do it, then come back to the step!)
 
-# notes
-oh and just to note here - the color scheme "vibe" is very much a work in progress. eventually there will be a pink on black theme, but i genuinely forgot i had intended vibe to be that so it's not. that. oops.
-with that in mind, i'm like, *sort of* open to color scheme suggestions, but i'm not likely to incorporate them unless a. i like them and/or b. you make it yourself (forking the repo and adding to colors.ini, or even just giving me like. actual hex codes idk.)
+# color schemes
+included:
+- <code>cybermoth</code> (default, dark): light green and pink on dark purple
+- <code>moth-to-flame</code> (dark): gold on dark, warm purple
+- <code>moonlit-waves</code> (dark): blue on dark, cool purple
+- <code>vibe</code> (dark): pink and blue on dark purple? WIP
+- <code>sun-at-night</code> (midnight): gold on black. WIP - currently no tint value
+- <code>leaf</code> (medium): soft greens
+- <code>beyond-our-stars</code> (medium): soft periwinkle with light pinky purple and blue
+
+potential future ideas:
+- pink and black
+- something orange
+
+i'm like, *sort of* open to color scheme suggestions, but i'm not likely to incorporate them unless a. i like them and/or b. you make it yourself (forking the repo and adding to colors.ini, or even just giving me like. actual hex codes idk.)
+
+# bugs
+if spicetify is not acting correctly while using this theme, please let me know by making an issue!
+i sometimes neglect to update my spicetify for a long time so i sometimes miss breaking API changes.
--- theme.js
+++ theme.js
@@ -12,32 +12,87 @@
     tintValues.set('moth-to-flame', [3.00, '-115.75deg', 0.90]);
     tintValues.set('moonlit-waves', [1.75, '52.75deg', 0.80]);
     tintValues.set('sun-at-night', [3.00, '-115.75deg', 0.90]);
-    tintValues.set('vibe', [3.00, '-115.75deg', 0.90]);
+
+    // placeholder tint values
+    tintValues.set('vibe', [3.00, '-115.75deg', 0.90]); // moth-to-flame
+    tintValues.set('leaf', [1.75, '-52.75deg', 0.58]); // cybermoth
+    tintValues.set('beyond-our-stars', [1.75, '52.75deg', 0.80]); // moonlit-waves
+
+    const defaultCoverArt = "spotify:image:ab67616d00001e02382ddf73e0132cecf399c718";
 
     /**
-     * Sets the tint values in the CSS.
-     * Will quit if it doesn't have a value stored for the color_scheme.
+     * Sets the CSS properties for tint values based on the color scheme.
+     * If the tint values cannot be found, this function does nothing.
      * @returns
      */
-    function assignTintValues() {
+    function setFilterProperties() {
         //checks if key exists - if not, let it default to however css assigns it
         if (!tintValues.has(Spicetify.Config.color_scheme)) {
+            warn("no filter property values found for " + Spicetify.Config.color_scheme);
             return;
         }
         //key exists, so we use that!
         document.documentElement.style.setProperty('--spice-filter-brightness', tintValues.get(Spicetify.Config.color_scheme)[0]);
         document.documentElement.style.setProperty('--spice-filter-hue-rotate', tintValues.get(Spicetify.Config.color_scheme)[1]);
         document.documentElement.style.setProperty('--spice-filter-saturation', tintValues.get(Spicetify.Config.color_scheme)[2]);
+
+        log("filter properties set for " + Spicetify.Config.color_scheme);
     }
 
     /**Sets the Lyrics background to the album cover of the current song.*/
     function setLyricsBackgroundImage() {
 
         // grab the current song's album cover via track metadata, and sets it as the lyrics background image url
-        document.documentElement.style.setProperty('--spice-imgurl-lyrics', "url("+Spicetify.Player.data.item.album.images[3].url+")");
+        document.documentElement.style.setProperty('--spice-imgurl-lyrics', "url("+getAlbumCover()+")");
     }
 
-    /**Gets everything up and running!*/
+    /**
+     * Fetches the URI of the album cover of the currently playing song.
+     * @returns {String}
+     * */
+    function getAlbumCover() {
+        // Preferred method: Fetch image from song metadata.
+
+        // OUTDATED?
+        // Use Spicetify Player to get to the currently playing song.
+        if (Spicetify.Player && Spicetify.Player.data) {
+            log("fetching album cover from Player");
+            return Spicetify.Player.data.item.album.images[3].url;
+        }
+
+        // QUEUE is described in docs, but doesn't seem to be implemented.
+        // If that fails, try getting it from the queue
+        else if (Spicetify.Queue) {
+            log("fetching album cover from Queue");
+            // This is type ProvidedTrack which extends ContextTrack.
+            const track = Spicetify.Queue.track;
+            // Access the metadata of the ContextTrack to get the large image.
+            return track.metadata.image_large_url;
+        }
+
+        // If all else fails, grab image from the playbar.
+        else {
+            log("fetching album cover from playbar");
+            // Retrieve album cover elements. These are img elements whose src is the album art.
+            const coverArtImages = document.getElementsByClassName("cover-art-image");
+
+            // If there are cover art elements, grab one.
+            if (coverArtImages.length > 0) {
+                // There may be multiple - just choose the first one.
+                return coverArtImages[0].src;
+            }
+            // There is no song playing right now - return the default cover art URI.
+            else {
+                warn("failed to fetch album cover, using default cover art");
+                return defaultCoverArt;
+            }
+        }
+    }
+
+    /**
+     * Gets everything up and running!
+     * Waits until the Spicetify API objects it needs are ready, then assigns tint values and adds the event listener for song changes.
+     * */
     function init(delay = 100) {
         // make sure we're ready to start
         if (!Spicetify.Config || !Spicetify.Player) {
@@ -45,21 +100,28 @@
             setTimeout(init, delay + 100);
             return;
         }
-        assignTintValues();
+        setFilterProperties();
 
         // If there's a song open already, load that album cover bg!
+        // TODO: Current song is no longer in Player. Come up with some workaround
         if (Spicetify.Player.data) setLyricsBackgroundImage();
         else { 
-            console.log("vamptify: setting timeout to load lyrics background image")
+            log("setting timeout to load lyrics background image")
             setTimeout(setLyricsBackgroundImage, delay + 800) }
 
         // Set up event listener for the lyrics background :>
         Spicetify.Player.addEventListener("songchange", () => { setLyricsBackgroundImage() });
     }
 
-    // Init sets its own timeout, so hopefully we won't have to mess with it!
-    init();
+    /** Shorthand for console.log that prefixes logs with a signature. */
+    function log(msg) {
+        console.log("vamptify: " + msg);
+    }
+    /** Shorthand for console.warn that prefixes logs with a signature. */
+    function warn(msg) {
+        console.warn("vamptify: " + msg);
+    }
 
-    // Init is on a timeout to make sure everything can start up first.
-    //setTimeout(init, 500);
+    // Init sets its own timeout, can safely be called at any time.
+    init();
 })();
